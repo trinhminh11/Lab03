23c04402709e6e9ec7201aa788cb9ecd62ec2edb
trinhminh11 <72061760+trinhminh11@users.noreply.github.com>
2024-04-24 10:51:36 +0700

    some fix, rmv

--- src/TestPassingParameter.java
+++ src/TestPassingParameter.java
@@ -45,19 +45,6 @@ public class TestPassingParameter {
 		changeTitle(jungleDVD, cinderellaDVD.getTitle());
 		System.out.println("jungle dvd title " + jungleDVD.getTitle());
 	}
-	
-
-	public static void swap(Object o1, Object o2){
-		Object tmp = o1;
-		o1 = o2;
-		o2 = tmp;
-	}
-
-	public static void TrueSwap(DigitalVideoDisc d1, DigitalVideoDisc d2){
-		DigitalVideoDisc tmp = d1;
-		d1.DeepCopy(d2);
-		d2.DeepCopy(tmp);
-	}
 
 	public static void changeTitle(DigitalVideoDisc dvd, String tile){
 		String oldTitle = dvd.getTitle();
