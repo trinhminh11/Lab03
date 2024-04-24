06aa2dc49d98b857a3002118c7acda2ab5b00861
trinhminh11 <72061760+trinhminh11@users.noreply.github.com>
2024-04-24 10:51:23 +0700

    create class Pair to swap any Object with the same type

--- src/TestPassingParameter.java
+++ src/TestPassingParameter.java
@@ -1,12 +1,44 @@
+
 public class TestPassingParameter {
+
+	public static class Pair <T>{
+		private T o1;
+		private T o2;
+
+		public Pair(T o1, T o2){
+			this.o1 = o1;
+			this.o2 = o2;
+		}
+
+		public void swap(){
+			T tmp = o1;
+			o1 = o2;
+			o2 = tmp;
+		}
+
+		public T first(){
+			return o1;
+		}
+
+		public T second(){
+			return o2;
+		}
+	}
 
 
 	public static void main(String[] args) {
 		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
 		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
+		Pair<DigitalVideoDisc> pair = new Pair<DigitalVideoDisc>(jungleDVD, cinderellaDVD);
 
-		TrueSwap(jungleDVD, cinderellaDVD);
+		pair.swap();
+
+		jungleDVD = pair.first();
+		cinderellaDVD = pair.second();
+		
+		
+
 		System.out.println("jungle dvd title " + jungleDVD.getTitle());
 		System.out.println("cinderella dvd title " + cinderellaDVD.getTitle());
 
