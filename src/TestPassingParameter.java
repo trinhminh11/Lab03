d535c3d31c77fb8947bfb797332b9c4cb11b933d
trinhminh11 <72061760+trinhminh11@users.noreply.github.com>
2024-04-24 10:52:32 +0700

    Fixing

--- src/TestPassingParameter.java
+++ src/TestPassingParameter.java
@@ -46,6 +46,13 @@ public class TestPassingParameter {
 		System.out.println("jungle dvd title " + jungleDVD.getTitle());
 	}
 
+	public static void swap(Object o1, Object o2){
+		Object tmp = o1;
+		o1 = o2;
+		o2 = tmp;
+	}
+
+
 	public static void changeTitle(DigitalVideoDisc dvd, String tile){
 		String oldTitle = dvd.getTitle();
 		dvd.setTitle(tile);
